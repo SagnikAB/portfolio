10838197b433ed3b46433ff1ea1a0d6f7f612222
Sagnik Dam <sagnikdam29@gmail.com>
2026-02-01 19:55:29 +0530

    Updated portfolio layout and dark/light mode

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function toggleProjects() {
-  const section = document.getElementById("projects");
-  section.classList.toggle("hidden");
+function toggleProjects(){
+  document.getElementById("projects").classList.toggle("hidden");
 }
 
 const toggle = document.getElementById("modeToggle");
+
 toggle.addEventListener("change", () => {
   document.body.classList.toggle("light");
 });
